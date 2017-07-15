fe3b5ed3ab3269b49250a0010d039c022ff91b3e
isoyoung <netwitoz@gmail.com>
2017-07-15 17:58:39 +0900

    Postviewでダウンロードした画像の表示をできるようにした

--- app/src/main/assets/remote_controller.js
+++ app/src/main/assets/remote_controller.js
@@ -4,11 +4,30 @@
         shoot();
       }
 
-      function callback(text){
-        var result=document.getElementById("result");
-        result.value=text
+      // Postview画像を表示する
+      function show_image(url){
+        //document.write("<img src=" + url + " width=100 height=100>");
+        var img=document.getElementById("result_image");
+        img.src=url;
       }
 
+      function callback(result){
+//        var result=document.getElementById("result");
+//        result.value=text.
+
+          var result_area = document.getElementById("result");
+          result_area.value = extract_url(result);
+
+          show_image(extract_url(result));
+      }
+
+      // 結果からURLだけを抽出する
+      function extract_url(result){
+        var json = JSON.parse(result);
+        return json["result"]
+      }
+
+      // 写真の撮影をする
       function shoot() {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
